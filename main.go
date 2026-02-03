4d2dc5eba3d3112059a254450dd653d141c6b7e7
dknetwell <dknetwell@gmail.com>
2026-02-03 14:47:13 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ import (
     "os"
     "os/signal"
     "syscall"
-    
+
     "github.com/coredns/coredns/core/dnsserver"
     "github.com/coredns/coredns/coremain"
-    
-    _ "github.com/dknetwell/dnscloud-go/plugin/security"
-    
-    "github.com/dknetwell/dnscloud-go/config"
-    "github.com/dknetwell/dnscloud-go/logger"
+
+    _ "plugin/security"
+
+    "config"
+    "logger"
 )
 
 func init() {
@@ -19,10 +19,10 @@ func init() {
     if err := config.Load(); err != nil {
         panic(err)
     }
-    
+
     // Инициализация логгера
     logger.Init(config.Get().Logging)
-    
+
     // Регистрация плагина
     dnsserver.Directives = append(dnsserver.Directives, "security")
 }
@@ -31,14 +31,14 @@ func main() {
     // Захват сигналов для graceful shutdown
     sigChan := make(chan os.Signal, 1)
     signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-    
+
     go func() {
         <-sigChan
         logger.Info("Shutdown signal received")
         // Cleanup логика
         os.Exit(0)
     }()
-    
+
     // Запуск CoreDNS
     coremain.Run()
 }
