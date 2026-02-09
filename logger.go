1e50fc869bf15268504694e76a827a80cb107bf2
dknetwell <dknetwell@gmail.com>
2026-02-10 00:30:58 +0300

    Create logger.go

---
+++ logger.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "time"
+)
+
+type logLevel int
+
+const (
+    debugLevel logLevel = iota
+    infoLevel
+    warnLevel
+    errorLevel
+)
+
+var (
+    currentLevel logLevel
+    logOutput    = os.Stdout
+)
+
+func initLogger(level, format string) {
+    switch level {
+    case "debug":
+        currentLevel = debugLevel
+    case "info":
+        currentLevel = infoLevel
+    case "warn":
+        currentLevel = warnLevel
+    case "error":
+        currentLevel = errorLevel
+    default:
+        currentLevel = infoLevel
+    }
+}
+
+func logDebug(msg string, args ...interface{}) {
+    if currentLevel <= debugLevel {
+        log("DEBUG", msg, args...)
+    }
+}
+
+func logInfo(msg string, args ...interface{}) {
+    if currentLevel <= infoLevel {
+        log("INFO", msg, args...)
+    }
+}
+
+func logWarn(msg string, args ...interface{}) {
+    if currentLevel <= warnLevel {
+        log("WARN", msg, args...)
+    }
+}
+
+func logError(msg string, err error, args ...interface{}) {
+    if currentLevel <= errorLevel {
+        allArgs := append([]interface{}{"error", err}, args...)
+        log("ERROR", msg, allArgs...)
+    }
+}
+
+func log(level, msg string, args ...interface{}) {
+    timestamp := time.Now().Format("2006-01-02T15:04:05.000Z07:00")
+    fmt.Fprintf(logOutput, "%s [%s] %s", timestamp, level, msg)
+    
+    if len(args) > 0 {
+        for i := 0; i < len(args); i += 2 {
+            if i+1 < len(args) {
+                fmt.Fprintf(logOutput, " %v=%v", args[i], args[i+1])
+            }
+        }
+    }
+    fmt.Fprintln(logOutput)
+}
