e012148b848aecf2633454922ad9407b480f17ef
dknetwell <dknetwell@gmail.com>
2026-02-17 19:59:57 +0300

    Update dns_handler.go

--- dns_handler.go
+++ dns_handler.go
@@ -7,87 +7,144 @@ import (
 	"time"
 
 	"github.com/miekg/dns"
-	"golang.org/x/sync/singleflight"
 )
 
-const maxDNSPacketSize = 1232
-
 type DNSServer struct {
-	engine       *CheckEngine
-	resolver     *net.Resolver
-	sf           singleflight.Group
-	sinkholeIPv4 net.IP
-	sinkholeIPv6 net.IP
+	engine *CheckEngine
+	cfg    *Config
 }
 
 func NewDNSServer(engine *CheckEngine, cfg *Config) *DNSServer {
-
 	return &DNSServer{
-		engine:       engine,
-		resolver:     netResolver,
-		sinkholeIPv4: net.ParseIP(cfg.DNS.SinkholeIPv4),
-		sinkholeIPv6: net.ParseIP(cfg.DNS.SinkholeIPv6),
+		engine: engine,
+		cfg:    cfg,
 	}
 }
 
-func (s *DNSServer) HandleDNS(w dns.ResponseWriter, r *dns.Msg) {
+func (s *DNSServer) Start() {
 
-	if r.Len() > maxDNSPacketSize {
+	dns.HandleFunc(".", s.handleDNS)
+
+	udpServer := &dns.Server{
+		Addr:         s.cfg.DNS.ListenUDP,
+		Net:          "udp",
+		UDPSize:      uint16(s.cfg.DNS.MaxPacketSize),
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 2 * time.Second,
+	}
+
+	tcpServer := &dns.Server{
+		Addr:         s.cfg.DNS.ListenTCP,
+		Net:          "tcp",
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 2 * time.Second,
+	}
+
+	go udpServer.ListenAndServe()
+	go tcpServer.ListenAndServe()
+
+	LogInfo("dns", "DNS server started")
+}
+
+func (s *DNSServer) handleDNS(w dns.ResponseWriter, r *dns.Msg) {
+
+	if r.Len() > s.cfg.DNS.MaxPacketSize {
 		return
 	}
 
-	m := new(dns.Msg)
-	m.SetReply(r)
-
-	for _, q := range r.Question {
-
-		domain := strings.TrimSuffix(q.Name, ".")
-
-		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-		result, err := s.engine.CheckDomain(ctx, domain)
-		cancel()
-
-		if err != nil {
-			continue
-		}
-
-		switch q.Qtype {
-
-		case dns.TypeA:
-			ip := result.RealIP
-			if result.Blocked {
-				ip = s.sinkholeIPv4
-			}
-			if ip != nil {
-				m.Answer = append(m.Answer, &dns.A{
-					Hdr: dns.RR_Header{
-						Name:   q.Name,
-						Rrtype: dns.TypeA,
-						Class:  dns.ClassINET,
-						Ttl:    uint32(result.TTL),
-					},
-					A: ip,
-				})
-			}
-
-		case dns.TypeAAAA:
-			ip := result.RealIPv6
-			if result.Blocked {
-				ip = s.sinkholeIPv6
-			}
-			if ip != nil {
-				m.Answer = append(m.Answer, &dns.AAAA{
-					Hdr: dns.RR_Header{
-						Name:   q.Name,
-						Rrtype: dns.TypeAAAA,
-						Class:  dns.ClassINET,
-						Ttl:    uint32(result.TTL),
-					},
-					AAAA: ip,
-				})
-			}
-		}
+	if len(r.Question) == 0 {
+		return
 	}
 
-	_ = w.WriteMsg(m)
+	q := r.Question[0]
+	domain := strings.TrimSuffix(q.Name, ".")
+
+	result, _ := s.engine.CheckDomain(domain)
+
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Authoritative = true
+
+	if result.Blocked {
+		s.writeSinkhole(m, q)
+	} else {
+		s.resolveReal(m, q, result)
+	}
+
+	w.WriteMsg(m)
+}
+
+func (s *DNSServer) writeSinkhole(m *dns.Msg, q dns.Question) {
+
+	switch q.Qtype {
+
+	case dns.TypeA:
+		rr := &dns.A{
+			Hdr: dns.RR_Header{
+				Name:   q.Name,
+				Rrtype: dns.TypeA,
+				Class:  dns.ClassINET,
+				Ttl:    60,
+			},
+			A: net.ParseIP(s.cfg.DNS.SinkholeIPv4),
+		}
+		m.Answer = append(m.Answer, rr)
+
+	case dns.TypeAAAA:
+		rr := &dns.AAAA{
+			Hdr: dns.RR_Header{
+				Name:   q.Name,
+				Rrtype: dns.TypeAAAA,
+				Class:  dns.ClassINET,
+				Ttl:    60,
+			},
+			AAAA: net.ParseIP(s.cfg.DNS.SinkholeIPv6),
+		}
+		m.Answer = append(m.Answer, rr)
+
+	default:
+		m.Rcode = dns.RcodeSuccess
+	}
+}
+
+func (s *DNSServer) resolveReal(m *dns.Msg, q dns.Question, result *DomainResult) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	switch q.Qtype {
+
+	case dns.TypeA:
+		ips, _ := netResolver.LookupIP(ctx, "ip4", result.Domain)
+		for _, ip := range ips {
+			rr := &dns.A{
+				Hdr: dns.RR_Header{
+					Name:   q.Name,
+					Rrtype: dns.TypeA,
+					Class:  dns.ClassINET,
+					Ttl:    uint32(result.TTL),
+				},
+				A: ip,
+			}
+			m.Answer = append(m.Answer, rr)
+		}
+
+	case dns.TypeAAAA:
+		ips, _ := netResolver.LookupIP(ctx, "ip6", result.Domain)
+		for _, ip := range ips {
+			rr := &dns.AAAA{
+				Hdr: dns.RR_Header{
+					Name:   q.Name,
+					Rrtype: dns.TypeAAAA,
+					Class:  dns.ClassINET,
+					Ttl:    uint32(result.TTL),
+				},
+				AAAA: ip,
+			}
+			m.Answer = append(m.Answer, rr)
+		}
+
+	default:
+		m.Rcode = dns.RcodeSuccess
+	}
 }
