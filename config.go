8b6963b0b2c24b1c8c41c46d84d0d5adbb10ddc4
dknetwell <dknetwell@gmail.com>
2026-02-17 19:50:23 +0300

    Update config.go

--- config.go
+++ config.go
@@ -7,10 +7,19 @@ import (
 )
 
 type Config struct {
+
+	Logging struct {
+		Level  string `yaml:"level"`
+		Syslog bool   `yaml:"syslog"`
+	} `yaml:"logging"`
+
 	DNS struct {
-		Upstream     []string `yaml:"upstream"`
-		SinkholeIPv4 string   `yaml:"sinkhole_ipv4"`
-		SinkholeIPv6 string   `yaml:"sinkhole_ipv6"`
+		ListenUDP     string   `yaml:"listen_udp"`
+		ListenTCP     string   `yaml:"listen_tcp"`
+		Upstream      []string `yaml:"upstream"`
+		SinkholeIPv4  string   `yaml:"sinkhole_ipv4"`
+		SinkholeIPv6  string   `yaml:"sinkhole_ipv6"`
+		MaxPacketSize int      `yaml:"max_packet_size"`
 	} `yaml:"dns"`
 
 	CloudAPI struct {
@@ -19,6 +28,7 @@ type Config struct {
 		InsecureSkipVerify bool    `yaml:"insecure_skip_verify"`
 		RateLimit          float64 `yaml:"rate_limit"`
 		Burst              int     `yaml:"burst"`
+		TimeoutSeconds     int     `yaml:"timeout_seconds"`
 	} `yaml:"cloud_api"`
 
 	TTL struct {
@@ -31,21 +41,31 @@ type Config struct {
 		MaxCost int64 `yaml:"max_cost"`
 	} `yaml:"cache"`
 
+	Valkey struct {
+		Address  string `yaml:"address"`
+		Password string `yaml:"password"`
+		DB       int    `yaml:"db"`
+	} `yaml:"valkey"`
+
 	Engine struct {
 		WorkerCount     int `yaml:"worker_count"`
 		WorkerQueueSize int `yaml:"worker_queue_size"`
 	} `yaml:"engine"`
+
+	HTTP struct {
+		Listen string `yaml:"listen"`
+	} `yaml:"http"`
 }
 
 func LoadConfig(path string) (*Config, error) {
 
-	file, err := os.ReadFile(path)
+	data, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
 	var cfg Config
-	if err := yaml.Unmarshal(file, &cfg); err != nil {
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
 		return nil, err
 	}
 
