57fd1731d2ec153b78ca0de3d24d80500425c731
dknetwell <dknetwell@gmail.com>
2026-02-17 18:16:48 +0300

    Update metrics.go

--- metrics.go
+++ metrics.go
@@ -1,83 +1,41 @@
 package main
 
 import (
-    "sync"
-    "time"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-// MetricsCollector - сборщик метрик
-type MetricsCollector struct {
-    mu                sync.RWMutex
-    totalRequests     int64
-    cacheHits         int64
-    cacheMisses       int64
-    apiCalls          int64
-    slaViolations     int64
-    totalResponseTime time.Duration
-}
+var (
+	dnsRequests = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "dns_requests_total",
+		})
 
-func newMetricsCollector() *MetricsCollector {
-    return &MetricsCollector{}
-}
+	dnsLatency = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Name:    "dns_latency_seconds",
+			Buckets: prometheus.DefBuckets,
+		})
 
-func (m *MetricsCollector) incCacheHit() {
-    m.mu.Lock()
-    defer m.mu.Unlock()
-    m.cacheHits++
-    m.totalRequests++
-}
+	cacheHits = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "dns_cache_hits_total",
+		})
+)
 
-func (m *MetricsCollector) incCacheMiss() {
-    m.mu.Lock()
-    defer m.mu.Unlock()
-    m.cacheMisses++
-    m.totalRequests++
-}
-
-func (m *MetricsCollector) incAPICall() {
-    m.mu.Lock()
-    defer m.mu.Unlock()
-    m.apiCalls++
-}
-
-func (m *MetricsCollector) incTimeout() {
-    m.mu.Lock()
-    defer m.mu.Unlock()
-    m.slaViolations++
-}
-
-func (m *MetricsCollector) recordCheckDuration(duration time.Duration, source string) {
-    m.mu.Lock()
-    defer m.mu.Unlock()
-    m.totalResponseTime += duration
-    
-    if source == "cloud_api" {
-        m.apiCalls++
-    }
-}
-
-func (m *MetricsCollector) getStats() *Stats {
-    m.mu.RLock()
-    defer m.mu.RUnlock()
-    
-    var avgResponseTime time.Duration
-    if m.totalRequests > 0 {
-        avgResponseTime = m.totalResponseTime / time.Duration(m.totalRequests)
-    }
-    
-    return &Stats{
-        TotalRequests:    m.totalRequests,
-        CacheHits:        m.cacheHits,
-        CacheMisses:      m.cacheMisses,
-        APICalls:         m.apiCalls,
-        SLAViolations:    m.slaViolations,
-        AvgResponseTime:  avgResponseTime,
-        ActiveConnections: 0, // TODO: добавить подсчет
-    }
-}
-
-// Инициализация метрик Prometheus (упрощенная версия)
 func initMetrics() {
-    // Здесь можно инициализировать Prometheus метрики
-    logDebug("Metrics initialized")
+	prometheus.MustRegister(dnsRequests, dnsLatency, cacheHits)
+}
+
+func incrementDNSCounter() {
+	dnsRequests.Inc()
+}
+
+func observeDNSLatency(d time.Duration) {
+	dnsLatency.Observe(d.Seconds())
+}
+
+func incrementCacheHit() {
+	cacheHits.Inc()
 }
