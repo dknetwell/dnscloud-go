072cd8065eb24cf99d4decbe9dd6aa636eb7d3d0
dknetwell <dknetwell@gmail.com>
2026-02-17 19:48:39 +0300

    Update logger.go

--- logger.go
+++ logger.go
@@ -1,51 +1,73 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"log/syslog"
-	"os"
-	"time"
+    "fmt"
+    "time"
 )
 
-var logger *log.Logger
+type logLevel int
 
-func InitLogger(cfg *Config) {
+const (
+    debugLevel logLevel = iota
+    infoLevel
+    warnLevel
+    errorLevel
+)
 
-	if cfg.Logging.Syslog {
-		writer, err := syslog.New(syslog.LOG_INFO|syslog.LOG_LOCAL0, "dns-proxy")
-		if err == nil {
-			logger = log.New(writer, "", 0)
-			return
-		}
-	}
+var (
+    currentLevel logLevel = infoLevel
+)
 
-	logger = log.New(os.Stdout, "", 0)
+func initLogger(level string) {
+    switch level {
+    case "debug":
+        currentLevel = debugLevel
+    case "info":
+        currentLevel = infoLevel
+    case "warn":
+        currentLevel = warnLevel
+    case "error":
+        currentLevel = errorLevel
+    default:
+        currentLevel = infoLevel
+    }
 }
 
-func logJSON(level, component, message string, fields map[string]interface{}) {
-
-	entry := map[string]interface{}{
-		"time":      time.Now().UTC(),
-		"level":     level,
-		"component": component,
-		"message":   message,
-	}
-
-	for k, v := range fields {
-		entry[k] = v
-	}
-
-	data, _ := json.Marshal(entry)
-	logger.Println(string(data))
+func logDebug(msg string, args ...interface{}) {
+    if currentLevel <= debugLevel {
+        log("DEBUG", msg, args...)
+    }
 }
 
-func LogInfo(component, message string) {
-	logJSON("info", component, message, nil)
+func logInfo(msg string, args ...interface{}) {
+    if currentLevel <= infoLevel {
+        log("INFO", msg, args...)
+    }
 }
 
-func LogError(component, message string, err error) {
-	logJSON("error", component, message, map[string]interface{}{
-		"error": err.Error(),
-	})
+func logWarn(msg string, args ...interface{}) {
+    if currentLevel <= warnLevel {
+        log("WARN", msg, args...)
+    }
+}
+
+func logError(msg string, err error, args ...interface{}) {
+    if currentLevel <= errorLevel {
+        allArgs := append([]interface{}{"error", err.Error()}, args...)
+        log("ERROR", msg, allArgs...)
+    }
+}
+
+func log(level, msg string, args ...interface{}) {
+    timestamp := time.Now().Format("2006-01-02 15:04:05")
+    fmt.Printf("%s [%s] %s", timestamp, level, msg)
+
+    if len(args) > 0 {
+        for i := 0; i < len(args); i += 2 {
+            if i+1 < len(args) {
+                fmt.Printf(" %v=%v", args[i], args[i+1])
+            }
+        }
+    }
+    fmt.Println()
 }
