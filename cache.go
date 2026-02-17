882a3b24ae2040e469cc3a0981ca1a5d05a97e38
dknetwell <dknetwell@gmail.com>
2026-02-17 19:53:20 +0300

    Update cache.go

--- cache.go
+++ cache.go
@@ -1,41 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/dgraph-io/ristretto"
 )
 
 type Cache struct {
-	store *ristretto.Cache
+	rc  *ristretto.Cache
+	cfg *Config
 }
 
 func NewCache(cfg *Config) *Cache {
 
-	cache, _ := ristretto.NewCache(&ristretto.Config{
+	rc, _ := ristretto.NewCache(&ristretto.Config{
 		NumCounters: 1e7,
 		MaxCost:     cfg.Cache.MaxCost,
 		BufferItems: 64,
 	})
 
-	return &Cache{store: cache}
+	return &Cache{
+		rc:  rc,
+		cfg: cfg,
+	}
 }
 
 func (c *Cache) Get(key string) (*DomainResult, bool) {
 
-	val, ok := c.store.Get(key)
+	v, ok := c.rc.Get(key)
 	if !ok {
 		return nil, false
 	}
-	return val.(*DomainResult), true
+
+	return v.(*DomainResult), true
 }
 
 func (c *Cache) Set(key string, value *DomainResult) {
 
-	cost := int64(len(key) + 64)
-	c.store.SetWithTTL(key, value, cost, time.Duration(value.TTL)*time.Second)
-}
+	data, _ := json.Marshal(value)
+	cost := int64(len(data))
 
-func (c *Cache) SetNegative(key string, ttl time.Duration) {
-	c.store.SetWithTTL(key, &DomainResult{Domain: key}, 1, ttl)
+	c.rc.SetWithTTL(key, value, cost,
+		time.Duration(value.TTL)*time.Second)
 }
