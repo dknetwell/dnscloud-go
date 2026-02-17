47cbbf307d305b3e2a4358c454e771d8b24055eb
dknetwell <dknetwell@gmail.com>
2026-02-17 19:50:41 +0300

    Update models.go

--- models.go
+++ models.go
@@ -10,9 +10,20 @@ type DomainResult struct {
 	Category  int       `json:"category"`
 	Action    string    `json:"action"`
 	TTL       int       `json:"ttl"`
+	Source    string    `json:"source"`
 	Timestamp time.Time `json:"timestamp"`
 
 	Blocked  bool   `json:"blocked"`
 	RealIP   net.IP `json:"-"`
 	RealIPv6 net.IP `json:"-"`
+	Negative bool   `json:"-"`
+}
+
+type Stats struct {
+	TotalRequests   int64         `json:"total_requests"`
+	CacheHits       int64         `json:"cache_hits"`
+	CacheMisses     int64         `json:"cache_misses"`
+	APICalls        int64         `json:"api_calls"`
+	EnrichmentQueue int           `json:"enrichment_queue"`
+	AvgLatency      time.Duration `json:"avg_latency"`
 }
